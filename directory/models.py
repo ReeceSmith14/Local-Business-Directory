6e4340428e2cc89c7912742aa7af7b2e8ebe8415
Reece Smith <reecesmith@hotmail.co.uk>
2024-08-21 20:41:50 +0000

    feat: business and category added to models.py

--- directory/models.py
+++ directory/models.py
@@ -9,17 +9,32 @@ class User(db.Model):
     email = db.Column(db.String(25), unique = True, nullable = False)
     password = db.Column(db.String(25), nullable = False)
 
+    businesses = db.relationship("Business", backref = "user", cascade = "all, delete", lazy = True)
+
     def __repr__(self):
-        return f"Name: {first_name} {last_name}, Username: {user_name}, Email: {email}, Password: {password}"
+        return f"{self.id} - Name: {self.first_name} {self.last_name}, Username: {self.user_name}"
 
 
 
 class Business(db.Model):
     id = db.Column(db.Integer, primary_key = True)
+    business_name = db.Column(db.String(25), unique = True, nullable = False)
+    business_description = db.Column(db.String(255), nullable = False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete = "CASCADE"), nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete = "CASCADE"), nullable = False)
+    phone = db.Column(db.String(11), unique=True, nullable=True)
+    email = db.Column(db.String(255), unique = True, nullable = True)
+    website = db.Column(db.String(255), unique = True, nullable = True)
+    image_url = db.Column(db.String(255), nullable=True) 
 
-
-
+    def __repr__(self):
+        return f"{self.id} - Business Name: {self.business_name}"
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key = True)
+    category_name = db.Column(db.String(25), unique = True, nullable = False)
+    
+    businesses = db.relationship("Business", backref = "category", cascade = "all, delete", lazy = True)
 
+    def __repr__(self):
+        return f"{self.id} - Category Name: {self.category_name}"
--- directory/routes.py
+++ directory/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from directory import app, db
+from directory.models import User, Business, Category
 
 @app.route("/")
 def home():
