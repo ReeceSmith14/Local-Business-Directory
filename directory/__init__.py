7314ac2914ebb836dc5ddf116fa2e7429ecffe01
Reece Smith <reecesmith@hotmail.co.uk>
2024-09-13 04:49:06 +0000

    chore: Application prepared from Heroku

--- directory/__init__.py
+++ directory/__init__.py
@@ -6,7 +6,14 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("postgres://u5kdylli8ft:6h4ydK3slSt7@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/lilac_task_chump_379279")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
 
