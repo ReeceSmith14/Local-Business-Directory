8bedea2d208d07f6bd1df440ee24198d5d5aad4f
Reece Smith <reecesmith@hotmail.co.uk>
2024-09-13 05:51:55 +0000

    fix: potential fix for Heroku

--- directory/__init__.py
+++ directory/__init__.py
@@ -10,7 +10,7 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-    uri = os.environ.get("postgres://u5kdylli8ft:6h4ydK3slSt7@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/lilac_task_chump_379279")
+    uri = "postgres://u5kdylli8ft:6h4ydK3slSt7@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/lilac_task_chump_379279"
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
